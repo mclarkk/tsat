0e45eaa4b19b54d3b37a83dd73fa628875c20419
Justin Bradley <justyn@umich.edu>
2012-11-30 15:23:04 -0500

    deleted test_i2c and modified test_pwm to be a loop

--- src/test_pwm.c
+++ src/test_pwm.c
@@ -23,27 +23,35 @@ int main () {
         pwminit();
 //if((fd = open("/dev/mem", O_RDWR | O_SYNC)) == -1) FATAL; //open the memory for changing the PWM
        	int pwm8duty, pwm9duty,pwm10duty,pwm11duty,choice;
-//        printf("Enter which pwm 8=servo, 10=thrust, 11=lift: ");
-        printf("Enter which pwm 10, or 11: ");
-        scanf("%d",&choice);getchar();
-        switch(choice) {
-	case 8:printf("Enter a number for 8: ");
-                scanf("%d",&pwm8duty);getchar();
-                pwm8(pwm8duty);
-                break;
-        case 9:printf("Enter a number for 9: ");
-                scanf("%d",&pwm9duty);getchar();
-                pwm9(pwm9duty);
-                break;
-        case 10:printf("Enter a number for 10: ");
-                scanf("%d",&pwm10duty);getchar();
-                pwm10(pwm10duty);
-                break;
-        case 11:printf("Enter a number for 11: ");
-                scanf("%d",&pwm11duty);getchar();
-                pwm11(pwm11duty);
-                break;
-        default:printf("Wrong Number\n");break;
+        char quit[5]; 
+        while(quit[0] != 'q')
+        {
+                printf("Enter which pwm 10=east, or 11=west, or 'q' for quit: ");
+                gets(quit)
+                if(quit[0] == 'q')
+                {
+                        break;
+                }
+                choice = atoi(&quit);
+                switch(choice) {
+                case 8:printf("Enter a number for 8: ");
+                        scanf("%d",&pwm8duty);getchar();
+                        pwm8(pwm8duty);
+                        break;
+                case 9:printf("Enter a number for 9: ");
+                        scanf("%d",&pwm9duty);getchar();
+                        pwm9(pwm9duty);
+                        break;
+                case 10:printf("Enter a number for 10: ");
+                        scanf("%d",&pwm10duty);getchar();
+                        pwm10(pwm10duty);
+                        break;
+                case 11:printf("Enter a number for 11: ");
+                        scanf("%d",&pwm11duty);getchar();
+                        pwm11(pwm11duty);
+                        break;
+                default:printf("Wrong Number\n");break;
+                }
         }
         return 0;
 }
